52c523a7ab2c4941c444f7f9e0fdc4cae164cd2d
eduardoAN <eduardo.nunes16@fatec.sp.gov.br>
2024-06-20 20:48:55 -0300

    set new player and enemy images, add enemy movement

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
+import random
 
 # Init pygame
 pygame.init()
 
 # Create a screen
-height = 400
+height = 600
 width = 600
 screen = pygame.display.set_mode((width, height))
 
@@ -15,14 +16,25 @@ pygame.display.set_icon(icon)
 
 # Player
 playerImage = pygame.image.load("player.png")
-playerX = 270
-playerY = 300
+playerX = width / 2 - 40
+playerY = height - 100
 playerXChange = 0
 playerYChange = 0
+
+# Enemy
+enemyImage = pygame.image.load("enemy.png")
+enemyX = random.randint(0, (width - 40))
+enemyY = random.randint(0, (width // 2))
+enemyXChange = 0.3
+enemyYChange = 40
 
 
 def player(x, y):
     screen.blit(playerImage, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImage, (x, y))
 
 
 # Game loop
@@ -32,16 +44,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        # Player movement
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 playerXChange = -0.2
             if event.key == pygame.K_RIGHT:
                 playerXChange = 0.2
 
-            if event.key == pygame.K_UP:
-                playerYChange = -0.2
-            if event.key == pygame.K_DOWN:
-                playerYChange = 0.2
+            # if event.key == pygame.K_UP:
+            #    playerYChange = -0.2
+            # if event.key == pygame.K_DOWN:
+            #    playerYChange = 0.2
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -52,6 +65,26 @@ while running:
     playerX += playerXChange
     playerY += playerYChange
 
+    # Player movement boundary
+    if playerX < 0:
+        playerX = 0
+    elif playerX >= width - 80:
+        playerX = width - 80
+
+    if playerY < 0:
+        playerY = 0
+    elif playerY >= height - 80:
+        playerY = height - 80
+
+    # Enemy movement + boundary
+    enemyX += enemyXChange
+
+    if enemyX < 0:
+        enemyXChange *= -1
+        enemyY += enemyYChange
+    elif enemyX >= width - 80:
+        enemyXChange *= -1
+        enemyY += enemyYChange
 
     # RGB in screen
     red = 0
@@ -62,4 +95,6 @@ while running:
     # playerX += 1
 
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
+
     pygame.display.update()
